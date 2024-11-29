2e97acfcfe2ba16484fed52a03773573968ae2fe
Aleksey Sokolkov <a.e.sokolkov@gmail.com>
2024-11-30 00:09:05 +0500

    feat: icons

---
+++ src/icons/icon-search.tsx
@@ -0,0 +1,21 @@
+function IconSearch() {
+  return (
+    <svg
+      fill="none"
+      height="24"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      />
+    </svg>
+  );
+}
+
+export default IconSearch;
