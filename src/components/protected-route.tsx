a8d8cc8c6a42d685319caee9d55dee5afed6b754
Aleksey Sokolkov <a.e.sokolkov@gmail.com>
2024-11-30 23:30:53 +0500

    fix: protected route children type

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -2,12 +2,12 @@ import { Navigate } from "react-router";
 import { AuthStatus, CURRENT_AUTH_STATUS, Path } from "../settings";
 
 type ProtectedRouteProps = {
-  child: React.ReactElement;
+  children: React.ReactElement;
 };
 
-function ProtectedRoute({ child }: ProtectedRouteProps) {
+function ProtectedRoute({ children }: ProtectedRouteProps) {
   const currentAuthStatus = CURRENT_AUTH_STATUS;
-  return currentAuthStatus === AuthStatus.Authorized ? child : <Navigate to={Path.Login} />;
+  return currentAuthStatus === AuthStatus.Authorized ? children : <Navigate to={Path.Login} />;
 }
 
 export default ProtectedRoute;
