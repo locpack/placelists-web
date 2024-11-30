b9cefe378791daa21b7e5c566455a849565f6e31
Aleksey Sokolkov <a.e.sokolkov@gmail.com>
2024-11-30 22:38:25 +0500

    fat: error page

---
+++ src/pages/error-page.tsx
@@ -0,0 +1,10 @@
+function ErrorPage() {
+  return (
+    <>
+      <h1 className="text_h1">Wut? How you found that page?</h1>
+      <p className="text_p">404 Error</p>
+    </>
+  );
+}
+
+export default ErrorPage;
