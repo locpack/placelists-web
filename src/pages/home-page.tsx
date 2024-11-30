e816c49bc1f03d6331ea78726f84661ec923a7ce
Aleksey Sokolkov <a.e.sokolkov@gmail.com>
2024-11-30 22:39:40 +0500

    feat: home page

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,5 +1,30 @@
+import { useNavigate } from "react-router";
+import Block from "../components/block";
+import Button from "../components/button";
+import IconLogin from "../icons/icon-login";
+import { AuthStatus, ButtonType, CURRENT_AUTH_STATUS, Path } from "../settings";
+
 function HomePage() {
-  return <>home</>;
+  const currentAuthStatus = CURRENT_AUTH_STATUS;
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <h1 className="text_h1">Hello, it's Placelists!</h1>
+      <Block header="Why?">
+        <p className="text_p">Here you can do a lot of stuff!</p>
+      </Block>
+      {currentAuthStatus !== AuthStatus.Authorized && (
+        <Button
+          text="Login"
+          type={ButtonType.Primary}
+          icon={<IconLogin />}
+          onClick={() => navigate(Path.Login)}
+        />
+      )}
+    </>
+  );
 }
 
 export default HomePage;
