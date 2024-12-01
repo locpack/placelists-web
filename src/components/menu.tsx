e4b6eda73e81480f01d6085869f73da36f85bff2
Aleksey Sokolkov <a.e.sokolkov@gmail.com>
2024-12-02 00:11:00 +0500

    feat: menu buttons

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,12 +1,16 @@
+import DiscoverIcon from "../icons/discover-icon";
+import ProfileIcon from "../icons/profile-icon";
+import SaveIcon from "../icons/save-icon";
+import { Path } from "../settings";
 import MenuButton from "./menu-button";
 
 function Menu() {
   return (
     <>
       <nav className="menu">
-        <MenuButton text="Discover" />
-        <MenuButton text="Saved" />
-        <MenuButton text="Profile" />
+        <MenuButton text="Discover" path={Path.Discover} icon={<DiscoverIcon />} />
+        <MenuButton text="Saved" path={Path.Saved} icon={<SaveIcon />} />
+        <MenuButton text="Profile" path={Path.Profile} icon={<ProfileIcon />} />
       </nav>
     </>
   );
