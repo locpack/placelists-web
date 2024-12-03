f40da5b4517e9a278403b29b9067ffc4090f4917
Aleksey Sokolkov <a.e.sokolkov@gmail.com>
2024-12-04 00:13:59 +0500

    feat: profile page template

--- src/pages/profile-page.tsx
+++ src/pages/profile-page.tsx
@@ -1,5 +1,16 @@
+import { USER } from "../settings";
+
 function ProfilePage() {
-  return <>profile</>;
+  const user = USER;
+
+  return (
+    <>
+      <header className="header">
+        <h1 className="text_h1">{user.name}</h1>
+        <h2 className="username text_h2">{user.username}</h2>
+      </header>
+    </>
+  );
 }
 
 export default ProfilePage;
