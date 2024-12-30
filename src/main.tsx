acd59ecb7a64444ed1d14433227a210abdaa6f72
Aleksey <a.e.sokolkov@gmail.com>
2024-12-30 19:15:01 +0500

    draft: github oauth

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import App from "./app";
 import "./index.css";
 import DiscoverPage from "./pages/discover-page";
 import ErrorPage from "./pages/error-page";
+import LoginPage from "./pages/login-page";
 import PlacelistPage from "./pages/placelist-page";
 import { Path } from "./settings";
 import { store } from "./store/main";
@@ -20,6 +21,7 @@ createRoot(document.getElementById("root")!).render(
           <Route path={Path.Home} element={<App />}>
             <Route path={Path.Discover} element={<DiscoverPage />} />
             <Route path={`${Path.Placelists}/:id`} element={<PlacelistPage />} />
+            <Route path={Path.Login} element={<LoginPage />} />
             {/* <Route path={Path.Profile} element={<ProfilePage />} /> */}
             <Route path="*" element={<ErrorPage />} />
           </Route>
