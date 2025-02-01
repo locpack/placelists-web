836e36c5fbe274ea1d8b6053d1dbc364a4ca5f98
Aleksey <a.e.sokolkov@gmail.com>
2025-02-01 17:09:07 +0500

    fix: simplified settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,26 +1,6 @@
-import { nanoid } from "nanoid";
-
-export enum AuthStatus {
-  Unknown,
-  NotAuthorized,
-  Authorized,
-}
-
-export const AUTH_STATUS = AuthStatus.NotAuthorized;
-
-export const GITHUB_OAUTH_CLIENT_ID = "Iv23liXhGDSKbk62rTnM";
-
 export const FRONTEND_URL = "http://localhost:8081";
-
 export const BACKEND_URL = "http://localhost:8082";
 export const REQUEST_TIMEOUT = 5000;
-export const TOKEN_KEY_NAME = "placelists-token";
-
-export enum ButtonType {
-  Primary,
-  Accent,
-  Tertiary,
-}
 
 export enum Path {
   Home = "/",
@@ -36,29 +16,9 @@ export enum Path {
 }
 
 export enum Namespace {
-  User = "User",
+  Users = "Users",
   Placelists = "Placelists",
-  Placelist = "Placelist",
-  Place = "Place",
-  Oauth = "Oauth",
+  Places = "Places",
 }
-
-export enum RequestStatus {
-  Pending,
-  Fulfilled,
-  Error,
-}
-
-export enum OauthProvider {
-  Github = "github",
-  Yandex = "yandex",
-}
-
-export const USER = {
-  id: nanoid(8),
-  name: "Aleksey",
-  username: "kfcgenius",
-  mail: "minecraft@gmail.com",
-};
 
 export const MENU_HIDDEN = false;
